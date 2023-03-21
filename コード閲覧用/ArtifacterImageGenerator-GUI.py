92c8511d60e6c0968c92f78f61864c495097e6a7
CrossDarkrix <crosseddarkrix@gmail.com>
2023-03-21 14:49:38 +0900

    Update

--- コード閲覧用/ArtifacterImageGenerator-GUI.py
+++ コード閲覧用/ArtifacterImageGenerator-GUI.py
@@ -13,6 +13,7 @@ import time
 import urllib.request
 from collections import Counter
 from decimal import Decimal, ROUND_HALF_UP
+from ImageBytes import ArtifactGrades, Artifactemotes, ArtifactBaseImages, ArtifactAssets, ArtifactConstellation, ArtifactRarelity
 from io import BytesIO
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 from PIL import ImageFile
