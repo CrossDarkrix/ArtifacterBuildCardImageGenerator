0c1f5dab4e14bd0108456266a0c7fbb081427cdc
CrossDarkrix <crosseddarkrix@gmail.com>
2024-02-19 01:41:07 +0900

    add README.md

---
+++ iOS/a-Shell/README.md
@@ -0,0 +1,5 @@
+# a-Shellでの使い方
+
+```install_artifacter.py```を実行すると完了後に「artifacter」コマンドが使えるようになります。
+
+または```python3 ArtifacterUI.py```と入力して実行するとそのままUIが立ち上がります。
---
+++ iOS/a-Shell/install_artifacter.py
@@ -0,0 +1,21 @@
+#!python3
+
+import os, subprocess, urllib.request
+
+script = "import os\n\nos.system('python3 {}'.format(os.path.join(os.environ.get('HOME'), 'Library', 'bin', 'ArtifacterUI.py')))"
+
+if os.path.exists(os.path.join(os.environ.get('HOME'), 'Library', 'bin')):
+    with open(os.path.join(os.environ.get('HOME'), 'Library', 'bin', 'artifacter'), 'w', encoding='utf-8') as f:
+        f.write(script)
+    file = urllib.request.urlopen('https://raw.githubusercontent.com/CrossDarkrix/ArtifacterBuildCardImageGenerator/master/iOS/a-Shell/ArtifacterUI.py').read()
+    with open(os.path.join(os.environ.get('HOME'), 'Library', 'bin', 'ArtifacterUI.py'), 'wb') as a:
+        a.write(file)
+    print('Done! run "artifacter"!')
+else:
+    subprocess.run('pip install -UI pip', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    with open(os.path.join(os.environ.get('HOME'), 'Library', 'bin', 'artifacter'), 'w', encoding='utf-8') as f:
+        f.write(script)
+    file = urllib.request.urlopen('https://raw.githubusercontent.com/CrossDarkrix/ArtifacterBuildCardImageGenerator/master/iOS/a-Shell/ArtifacterUI.py').read()
+    with open(os.path.join(os.environ.get('HOME'), 'Library', 'bin', 'ArtifacterUI.py'), 'wb') as a:
+        a.write(file)
+    print('Done! run "artifacter"!')
